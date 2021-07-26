0156361cc75761a0778478bb1f0d54ff6d5bdda6
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2021-07-26 12:58:54 +0300

    Better application of CSS file.

--- app.R
+++ app.R
@@ -67,64 +67,66 @@ title <- "My Data Portal"
 logo <- "logo.png" # should be placed in www folder in the project directory, and be png format.
 
 ui <- bootstrapPage('', # Can run print(ui) to see the HTML code for the website after running this.
-                 # This compounds the title with the logo on the left    
-      navbarPage(div(img(src=logo, height = 40, style = "position: absolute; top: 0; bottom:0; left: 0; right:0; 
-                         margin-top: auto; margin-bottom: auto; margin-left: 5px; margin-right: 50px;"), 
-                     title, style = "margin-left: 45px;"), 
-                 
-                 # This is for the browser tab
-                 windowTitle = tags$head(tags$link(rel = "icon", type = "image/png", href = logo),
-                                         tags$title(title)),
-                 
-                 # Select default tab when calling site
-                 selected = "Data Access",
-                 
-                 # Read code from ui.R
-                 tabPanel("Data Access",
-                          source(file.path("ui_code.R"),  local = TRUE)$value
-                 ),
-                 
-                 # 'Data Catalog' tab
-                 tabPanel("Data Catalog",
-                          div(class="outer",
-                              includeCSS("style.css"),
-                              p(" "),
-                              tabsetPanel(
-                                tabPanel("Data Sources",  
-                                         p(" "),
-                                         DT::dataTableOutput("datasources")),
-                                tabPanel("Datasets",  
-                                         p(" "),
-                                         DT::dataTableOutput("datasetdesc")),
-                                tabPanel("Variables", 
-                                         p(" "),
-                                         div(class = "indicatortable",
-                                             DT::dataTableOutput("dataoverview")
-                                         ))
-                              ) 
-                          ) 
-                 ),
-                 
-                 # About tab (can include more sections and useful links), see ?shiny::tags
-                 tabPanel("About",
-                          div(class = "AboutContent",
-                          h3("Purpose of the Portal"),
-                          p('A free Shiny Application to host, filter, aggregate and and download data in various formats. 
-                            It can be set up by anyone for any purpose and with any data using the source code and instructions (README.md) available on Github:'),
-                          a("Source Code", href = "https://github.com/SebKrantz/shiny-data-portal", target = "_blank"),
-                          h3("Details"),
-                          p("The app was built using the 'shiny' web-application framework in R (and some small custom HTML and CSS). 
-                             It is currently serviced with macroeconomic data for Uganda downloaded and with various sources using various R API packages (IMFData, wbstats, Rilostat, tradestatistics) as well as data from the Bank of Uganda. 
-                             The app was built by Sebastian Krantz (ODI Fellow in the Ugandan Ministry of Finance, Planning and Economic Development 2020/21).") 
-                          # Source code is available on github and released under a GPL 2.0 license (so it may be appropriated and adapted by other organizations under certain conditions). "),
-                          ),
-                          tags$footer(
-                            HTML("Copyright &copy; 2021, Sebastian Krantz")
-                          )
-                 )
-      )
-  )
-
+            
+            # This includes a CSS stylesheet 'style.css' setting the appearance of the app. This sheet can be edited separately.           
+            includeCSS("style.css"),           
+            
+            # This compounds the title with the logo on the left    
+            navbarPage(div(img(src = logo, height = 40, style = "position: absolute; top: 0; bottom:0; left: 0; right:0; 
+                 margin-top: auto; margin-bottom: auto; margin-left: 5px; margin-right: 50px;"), 
+                           title, style = "margin-left: 45px;"), 
+                       
+                       # This is for the browser tab
+                       windowTitle = tags$head(tags$link(rel = "icon", type = "image/png", href = logo),
+                                               tags$title(title)),
+                       
+                       # Select default tab when calling site
+                       selected = "Data Access",
+                       
+                       # Read code from ui.R
+                       tabPanel("Data Access",
+                                source(file.path("ui_code.R"),  local = TRUE)$value
+                       ),
+                       
+                       # 'Data Catalog' tab
+                       tabPanel("Data Catalog",
+                                div(class = "outer",
+                                    p(" "),
+                                    tabsetPanel(
+                                      tabPanel("Data Sources",  
+                                               p(" "),
+                                               DT::dataTableOutput("datasources")),
+                                      tabPanel("Datasets",  
+                                               p(" "),
+                                               DT::dataTableOutput("datasetdesc")),
+                                      tabPanel("Variables", 
+                                               p(" "),
+                                               div(class = "indicatortable",
+                                                   DT::dataTableOutput("dataoverview")
+                                               ))
+                                    ) 
+                                ) 
+                       ),
+                       
+                       # About tab (can include more sections and useful links), see ?shiny::tags
+                       tabPanel("About",
+                                div(class = "AboutContent",
+                                    h3("Purpose of the Portal"),
+                                    p('A free Shiny Application to host, filter, aggregate and and download data in various formats. 
+                    It can be set up by anyone for any purpose and with any data using the source code and instructions (README.md) available on Github:'),
+                                    a("Source Code", href = "https://github.com/SebKrantz/shiny-data-portal", target = "_blank"),
+                                    h3("Details"),
+                                    p("The app was built using the 'shiny' web-application framework in R (and some small custom HTML and CSS). 
+                     It is currently serviced with macroeconomic data for Uganda downloaded and with various sources using various R API packages (IMFData, wbstats, Rilostat, tradestatistics) as well as data from the Bank of Uganda. 
+                     The app was built by Sebastian Krantz (ODI Fellow in the Ugandan Ministry of Finance, Planning and Economic Development 2020/21).") 
+                                    # Source code is available on github and released under a GPL 2.0 license (so it may be appropriated and adapted by other organizations under certain conditions). "),
+                                ),
+                                tags$footer(
+                                  HTML("Copyright &copy; 2021, Sebastian Krantz")
+                                )
+                       )
+            )
+)
 
 # This reads the server side code in server.R and creates the server function.
 server <- function(input, output, session) {
